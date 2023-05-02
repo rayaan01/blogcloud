b7b74629c20750dabbd9abb29f11d6b3fd806752
Rayaan Hussain <mohammed.hussain@oslash.com>
2023-05-02 23:44:10 +0530

    fix: validate envs

--- src/utils/appSecrets.ts
+++ src/utils/appSecrets.ts
@@ -1,12 +1,19 @@
-const appEnv = process.env.STAGE ?? ''
+const validateEnv = (key: string): string | undefined => {
+    if (!process.env[key]) {
+        throw new Error(`Environment Variable "${key}" is not set`)
+    }
+    return process.env[key]
+}
+
+const appEnv = validateEnv('STAGE')
 
 export const appSecrets = {
     stage: appEnv,
     mainTable: `${appEnv}-main-table`,
     apiGatewayName: `${appEnv}-blogs-api`,
-    region: process.env.AWS_LOCAL_REGION ?? '',
-    account: process.env.AWS_ACCOUNT_ID ?? '',
-    issuer: process.env.AUTH_ISSUER ?? '',
-    audience: process.env.AUTH_AUDIENCE ?? '',
-    authSecret: process.env.AUTH_SECRET ?? ''
+    region: validateEnv('AWS_LOCAL_REGION'),
+    account: validateEnv('AWS_ACCOUNT_ID'),
+    issuer: validateEnv('AUTH_ISSUER'),
+    audience: validateEnv('AUTH_AUDIENCE'),
+    authSecret: validateEnv('AUTH_SECRET')
 }
