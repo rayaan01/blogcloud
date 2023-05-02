e935d45f879be4cff550ab7f7154c6a38ff0488e
Rayaan Hussain <mohammed.hussain@oslash.com>
2023-05-03 00:59:45 +0530

    fix: better types for arguments middleware

--- src/lib/middlewares/validateArgumentsMiddleware.ts
+++ src/lib/middlewares/validateArgumentsMiddleware.ts
@@ -2,10 +2,16 @@ import { MiddlewareObj, Request } from '@middy/core'
 import createHttpError from 'http-errors'
 import { Struct, assert } from 'superstruct'
 import { httpResponses } from '../../utils/httpResponses'
+import { APIGatewayProxyEventV2WithLambdaAuthorizer } from 'aws-lambda'
+import { AuthContextSchemaType } from '../schema/AuthContextSchema'
 
-export const validateArgumentsMiddleware = <TStruct1, TStruct2>({ schema }: { schema: Struct<TStruct1, TStruct2> }): MiddlewareObj => {
+type Event = Omit<APIGatewayProxyEventV2WithLambdaAuthorizer<AuthContextSchemaType>, 'body'> & {
+    body: Record<string, string>
+}
 
-    const validateArgumentsMiddlewareBase = (request: Request): void => {
+export const validateArgumentsMiddleware = <TStruct1, TStruct2>({ schema }: { schema: Struct<TStruct1, TStruct2> }): MiddlewareObj<Event> => {
+
+    const validateArgumentsMiddlewareBase = (request: Request<Event>): void => {
         const { body } = request.event
         if (!body) {
             throw createHttpError(400, httpResponses[400])
