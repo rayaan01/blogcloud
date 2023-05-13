dcc4e02254eeb7afe3903b5162cc7c7af6cae148
Rayaan Hussain <mohammed.hussain@oslash.com>
2023-05-14 00:40:07 +0530

    fix: lint and cusotomize tailwind

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,8 +1,11 @@
 const Login = () => {
     return (
-        <h1>
-            Login
-        </h1>
+        <div className="flex flex-row border-solid border-black border-2 w-1/2">
+            <div>
+                <label htmlFor="username">Username:</label>
+                <input type="text" name="username" id="username"/>
+            </div>
+        </div>
     )
 }
 
