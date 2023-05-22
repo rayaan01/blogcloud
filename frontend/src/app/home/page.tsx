cec2c53c99dd66b50686de0a35246840e18cb09d
Rayaan Hussain <mohammed.hussain@oslash.com>
2023-05-23 00:42:29 +0530

    fix: simple home page

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link'
 import type { FC } from 'react'
+import Profile from './components/Profile'
 
 const Home: FC = () => {
     return (
-        <h1>
-            BlogCloud
-        </h1>
+        <div className='h-screen bg-gray-300'>
+            <div className='flex justify-between items-center h-[7%] bg-gray-100'>
+                <Link href='/home' className='text-black text-3xl ml-8'>BlogCloud</Link>
+                <Profile user='Rayaan'/>
+            </div>
+        </div>
     )
 }
 
