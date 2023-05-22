7354d829c9e3089d25afd5d53a899007033cb8e6
Rayaan Hussain <mohammed.hussain@oslash.com>
2023-05-22 18:47:36 +0530

    fix: lint

--- backend/src/lib/middlewares/corsHeadersMiddleware.ts
+++ backend/src/lib/middlewares/corsHeadersMiddleware.ts
@@ -2,6 +2,7 @@ import { MiddlewareObj, Request } from '@middy/core'
 import { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2 } from 'aws-lambda'
 
 export const attachCorsHeadersMiddleware = (): MiddlewareObj<APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2> => {
+
     const attachCorsHeadersMiddlewareBase = (request: Request<APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2>): void => {
         const headers = {
             'Access-Control-Allow-Origin': request.event.headers.origin ?? 'https://fake-host.com',
