e817499ce961f4eafd2266179d029aab3b695405
Rayaan Hussain <mohammed.hussain@oslash.com>
2023-05-30 20:33:22 +0530

    fix: rename profile to settings

--- frontend/src/app/home/components/Settings.tsx
+++ frontend/src/app/home/components/Settings.tsx
@@ -5,12 +5,12 @@ import DownArrowImage from '../../../../public/down-arrow.svg'
 import AccountImage from '../../../../public/account.svg'
 import LogoutImage from '../../../../public/logout.svg'
 
-const Profile: FC = () => {
+const Settings: FC = () => {
     return (
-        <div className='text-black mr-8 hover:cursor-pointer flex justify-evenly relative group text-2xl rounded-lg'>
+        <div className='text-black mr-8 hover:cursor-pointer flex justify-evenly relative group text-2xl'>
             <button>Settings</button>
             <Image src={DownArrowImage} alt='DownArrow' height={20} width={20} className='m-1'/>
-            <div className='flex flex-col justify-around items-center absolute top-10 bg-gray-100 text-center w-40 opacity-0 group-hover:opacity-100'>
+            <div className='flex flex-col justify-around items-center absolute top-10 bg-gray-100 text-center w-40 opacity-0 group-hover:opacity-100 group-hover:shadow-md'>
                <Link href='/profile' className='w-full pb-2 pt-2 hover:bg-white flex justify-center items-center'>
                     <Image src={AccountImage} alt='Account' height={20} width={20} className='mr-2'/>
                     <span>Profile</span>
@@ -24,4 +24,4 @@ const Profile: FC = () => {
     )
 }
 
-export default Profile
+export default Settings
--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import type { FC } from 'react'
-import Profile from './components/Profile'
+import Settings from './components/Settings'
 
 const Home: FC = () => {
     return (
@@ -8,7 +8,7 @@ const Home: FC = () => {
             <div className='flex justify-between items-center h-[7%] bg-gradient-to-r from-gray-100 to-gray-300'>
                 <Link href='/home' className='text-black text-3xl ml-8'>BlogCloud</Link>
                 <span className='text-3xl text-blue-900'>Welcome, Rayaan</span>
-                <Profile/>
+                <Settings/>
             </div>
         </div>
     )
