f3ef38dade3b8b041d099d8b377b002d8c1dc2c9
Rayaan Hussain <mohammed.hussain@oslash.com>
2023-06-08 00:32:43 +0530

    fix: style better

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -20,8 +20,14 @@ const Home: FC = () => {
         <div className='h-screen bg-gray-300'>
             <div className='flex justify-between items-center h-[7%] bg-gradient-to-r from-gray-100 to-gray-300'>
                 <Link href='/home' className='text-black text-3xl ml-8'>BlogCloud</Link>
-                <span className='text-3xl text-blue-900'>Welcome, {user.firstName}</span>
-                <Settings/>
+                <span className='text-3xl text-blue-900'>
+                    Welcome, {user.firstName}
+                </span>
+                <Settings />
+            </div>
+            <div className='w-full bg-slate-100 p-2'>
+                <span className='text-lg'>You have no posts yet!</span>
+                <Link href='/create' className='text-lg ml-2 text-blue-500'>Create one</Link>
             </div>
         </div>
     )
