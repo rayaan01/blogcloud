af28eecdffd420b62ab026a288bb260b9771a978
Rayaan Hussain <mohammed.hussain@oslash.com>
2023-06-18 19:49:24 +0530

    fix: add app logo

---
+++ frontend/src/components/Cloud.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import CloudComponent from '../../public/cloud.svg'
+
+export const cloud = <Image 
+    src={CloudComponent} 
+    alt="App Logo" 
+    width={25} 
+    height={25} 
+    className="inline mr-2" 
+    />
