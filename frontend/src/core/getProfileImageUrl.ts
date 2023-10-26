a52d4fce76ec61f3b05b1690b934eba47f49f4f0
Rayaan Hussain <md.rayaan.h@gmail.com>
2023-10-26 13:58:21 +0100

    fix: debug

--- frontend/src/core/getProfileImageUrl.ts
+++ frontend/src/core/getProfileImageUrl.ts
@@ -1,6 +1,6 @@
 export const getProfileImageUrl = (email: string): string => {
     const domain = process.env.NEXT_PUBLIC_S3_URL
-    const path = encodeURIComponent(`USER#${email}.jpg`)
+    const path = `USER#${email}.jpg`
     const url = new URL(path, domain)
     console.log('url is', url)
     return url.href
